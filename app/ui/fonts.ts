c4682c7dc28a294b35d9b20bf10cc4d6cac0c950
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-23 01:40:34 +0530

    no more lusitanas

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,12 +2,3 @@ import { Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-type Font = {
-    weight: string[];
-    subsets: string[];
-};
-
-export const lusitana = {
-    weight: ['400', '700'],
-    subsets: ['latin']
-}
