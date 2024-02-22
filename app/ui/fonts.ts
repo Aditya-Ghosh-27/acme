4f1bcbf5a3c16ec790e9d9a56971f952b103a7af
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-23 01:27:32 +0530

    Revert "removed all secondary font entities from the project"
    This reverts commit aab79458ab75ca07608f0ae673bc866533ebd664.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,3 +2,12 @@ import { Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
+type Font = {
+    weight: string[];
+    subsets: string[];
+};
+
+export const lusitana = {
+    weight: ['400', '700'],
+    subsets: ['latin']
+}
