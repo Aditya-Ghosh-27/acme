8cb3e2e4962b05d3ff105ffe5d420c94a5bb59c2
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-02-23 19:40:41 +0530

    removed run time build errors

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,9 +14,15 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
-
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -45,7 +52,10 @@ export function Card({
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
-      <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl">
+      <p
+        className="
+          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl"
+      >
         {value}
       </p>
     </div>
